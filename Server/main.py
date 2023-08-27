4b0e720637f3b68b3f5a1a55b09695234c407d1e
Hiten Vats <hitenvats16@gmail.com>
2023-08-27 12:49:57 +0530

    added database basic code

--- Server/main.py
+++ Server/main.py
@@ -2,6 +2,8 @@ import os
 import uvicorn
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from fastapi.middleware.gzip import GZipMiddleware
+from database.prisma import prisma
 
 load_dotenv()
 
@@ -10,6 +12,24 @@ PORT = int(os.getenv('PORT'))
 app = FastAPI(title='Chatbot Api',
               description='This is server for ChatBot.',
               version='0.0.1')
+
+app.add_middleware(GZipMiddleware)
+
+@app.on_event('startup')
+async def startup():
+    try:
+        await prisma.connect()
+        print('Database Connected Succesfully')
+    except:
+        print('Database Not Connected')
+
+@app.on_event('shutdown')
+async def shutdown():
+    try:
+        await prisma.disconnect()
+        print('database disconnected successfully')
+    except:
+        print('database not disconnected')
 
 @app.get('/')
 def read_root():
