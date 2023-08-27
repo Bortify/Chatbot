d248432a015f4dfea8997fb1f5c8a4da5f6aad71
Hiten Vats <hitenvats16@gmail.com>
2023-08-27 14:38:51 +0530

    code refactoring

--- Server/main.py
+++ Server/main.py
@@ -1,19 +1,18 @@
-import os
 import uvicorn
 from fastapi import FastAPI
-from dotenv import load_dotenv
 from fastapi.middleware.gzip import GZipMiddleware
+
 from database.prisma import prisma
+from constants.server import serverConstants
 
-load_dotenv()
-
-PORT = int(os.getenv('PORT'))
 
 app = FastAPI(title='Chatbot Api',
               description='This is server for ChatBot.',
               version='0.0.1')
 
+
 app.add_middleware(GZipMiddleware)
+
 
 @app.on_event('startup')
 async def startup():
@@ -23,6 +22,7 @@ async def startup():
     except:
         print('Database Not Connected')
 
+
 @app.on_event('shutdown')
 async def shutdown():
     try:
@@ -31,11 +31,14 @@ async def shutdown():
     except:
         print('database not disconnected')
 
+
 @app.get('/')
 def read_root():
     return {
-        'message': 'Server is working' 
+        'message': 'Server is working'
     }
 
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=PORT, log_level="info")
+    uvicorn.run(
+        "main:app", port=serverConstants['PORT'], log_level="info", reload=True)
