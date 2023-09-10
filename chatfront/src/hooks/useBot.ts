62b1432b4630bf2131229ce11cb1841674887aa7
Hiten Vats <hitenvats16@gmail.com>
2023-09-10 22:40:04 +0530

    added chat state

--- chatfront/src/hooks/useBot.ts
+++ chatfront/src/hooks/useBot.ts
@@ -1,11 +1,16 @@
-import { useEffect,useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Socket } from 'socket.io-client'
 
 import { createSocket } from '../sockets'
 
+interface ChatProps {
+  message: string
+  author: 'AGENT' | 'CLIENT'
+}
+
 export default function useBot({ chatId }: { chatId: string }) {
   const [socket, setSocket] = useState<Socket>()
-
+  const [chat, setChat] = useState<ChatProps[]>([])
   useEffect(() => {
     const socketInit = createSocket('http://localhost:8080', { chatId })
     socketInit.connect()
@@ -16,6 +21,8 @@ export default function useBot({ chatId }: { chatId: string }) {
   }, [chatId])
 
   return {
-    socket
+    socket,
+    chat,
+    setChat,
   }
 }
