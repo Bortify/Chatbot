9e42ad8b6d8af3db0605df8c4a911fb3ac40f525
Hiten Vats <hitenvats16@gmail.com>
2023-10-31 01:35:35 +0530

    added be to env in chatfront

--- chatfront/src/config.ts
+++ chatfront/src/config.ts
@@ -1,3 +1,1 @@
-export const BACKEND_URL = import.meta.env.DEV
-  ? 'http://localhost:8080'
-  : 'http://localhost:8080'
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8080'
