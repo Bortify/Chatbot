fe2a274a1599beb5dd882a6182f475eefcfd473c
Hiten Vats <hitenvats16@gmail.com>
2023-10-31 11:04:26 +0530

    dummy commit

--- Dashboard/utils/query.ts
+++ Dashboard/utils/query.ts
@@ -1,7 +1,6 @@
 import { QueryClient, QueryKey } from '@tanstack/react-query'
 
 export const queryClient = new QueryClient()
-
 export function invalidate(key: QueryKey) {
   return queryClient.invalidateQueries({
     queryKey: key
